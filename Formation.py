7e3a6c498060cc3ce4999d3083cd540469c437ab
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-09-30 15:28:04 +0200

    added Formation.py

---
+++ Formation.py
@@ -0,0 +1,37 @@
+class Formation:
+    def __init__(self):
+        """ initrializes an empty formation of agents """
+
+        self.__agents = []
+        self.__longest = 0.0
+
+    def calc_gap(self):
+        """ calculates the minimal gap between two agents
+
+        Returns:
+            float: length of minimal gap rounded to 2 decimal places
+        """
+        
+        number_of_agents = len(self.__agents)
+        needed_space_proportion = 1.4
+        needed_space = self.__longest * needed_space_proportion
+
+        return round(needed_space / number_of_agents, 2)
+
+    def add_agent(self, agent):
+        """ adds an agent to the formation
+
+        Args:
+            agent (Agent): agent that is to be added
+        """
+
+        self.__agents.append(agent)
+
+        if agent.length > self.__longest:
+            self.__longest = agent.length
+
+    def clear(self):
+        """ resets the formation to its default values """
+
+        self.__agents = []
+        self.__longest = 0
