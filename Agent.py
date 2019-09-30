177b7632337689b7fdf8a18767662d563ddafe61
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-09-30 17:50:49 +0200

    added Agent.py

---
+++ Agent.py
@@ -0,0 +1,24 @@
+from Formation import Formation
+import json
+
+ATTRIBUTES_FILE = "./attributes.json"
+
+class Agent:
+    def __init__(self):
+        global ATTRIBUTES_FILE
+
+        self.__communication = None # TODO
+        self.__driver = None        # TODO
+        self.__formation = Formation()
+
+        with open(ATTRIBUTES_FILE) as attributes_file:
+            data = attributes_file.read()
+            attributes = json.loads(data)
+            
+            self.__id = attributes['id']
+            self.__length = attributes['length']
+
+    def listen_for_message(self, message):
+        pass # TODO
+
+Agent()
