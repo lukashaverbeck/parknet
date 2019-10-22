5b3a244af028fdebb22ac3c235452296f64eaa24
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-10-22 10:56:05 +0200

    created own thread for refreshing the front agent id

--- FrontAgentScanner.py
+++ FrontAgentScanner.py
@@ -1,9 +1,13 @@
 import cv2
 import time
+import threading
 import pyzbar.pyzbar as pyzbar
 
 
 class FrontAgentScanner:
+
+    REFRESH_INTERVAL = 2
+
     def __init__(self):
         """ initializes the scanner and starts to refresh the front agent ID """
 
@@ -11,23 +15,23 @@ class FrontAgentScanner:
         # TODO Pi-Kamera initialisieren statt Webkamera
         self.__camera = cv2.VideoCapture(0)
 
-        refresh_interval = 0.01
-
-        while True:
-            self.refresh()
-            time.sleep(refresh_interval)
+        refresh_thread = threading.Thread(target=self.refresh)
+        refresh_thread.start()
 
     def refresh(self) -> None:
-        """ updates the front agent ID based on the current video input """
-        
-        _, frame = self.__camera.read()
-        decoded_objects = pyzbar.decode(frame)
+        """ permanently updates the front agent ID based on the current video input """
 
-        if len(decoded_objects) > 0:
-            data_bytes = decoded_objects[0].data
-            self.__front_agent_id = data_bytes.decode("utf-8")
-        else:
-            self.__front_agent_id = None
+        while True:
+            _, frame = self.__camera.read()
+            decoded_objects = pyzbar.decode(frame)
+
+            if len(decoded_objects) > 0:
+                data_bytes = decoded_objects[0].data
+                self.__front_agent_id = data_bytes.decode("utf-8")
+            else:
+                self.__front_agent_id = None
+
+            time.sleep(self.REFRESH_INTERVAL)
 
     def get_front_agent_id(self) -> str:
         return self.__front_agent_id
