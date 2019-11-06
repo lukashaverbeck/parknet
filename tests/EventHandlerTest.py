c553377253686eb0b2817ca39a34f203896fd265
LukasGra <LukasGra@users.noreply.github.com>
2019-11-06 19:53:10 +0100

    new EventHandler

--- tests/EventHandlerTest.py
+++ tests/EventHandlerTest.py
@@ -1,17 +1,17 @@
-def test(x):
+def testhere(x):
     print("arg was "+str(x))
-toDoList = []
-args = ["hello"]
-toDoList.append({'f': test, 'a': args})
-# to run
-for item in toDoList:
-     item['f']("t")
 
-class EventHandlerTest():
-    def runEvent(self):
-        self
-    def registerInEvent(self, methodName):
-        print (methodName)
-        self
+methods = []
+args = ["unset"]
 
-LukasGra@users.noreply.github.com
+
+class EventHandlerTest:
+    def runEvent(message):
+        print ("Executing")
+        for item in methods:
+            item['f'](message)
+    def registerInEvent(methodName):
+        methods.append({'f': methodName, 'a': args})
+        ##print("Registered" + methodName)
+
+EventHandlerTest.registerInEvent(testhere)
