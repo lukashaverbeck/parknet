191a00d7b8000953c4bf9e8982457af3ea87fe51
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-11-08 17:14:41 +0100

    Update SensorManager.py
    Managing HC-SR04 modules and returning distance values.

--- vehicle/SensorManager.py
+++ vehicle/SensorManager.py
@@ -1,24 +1,28 @@
+# This class handles the three HC-SR04 modules connected to the Picar.
+# It is responsible for returning distance values requested by other classes. 
+
+# author:	@lukashaverbeck
+# author:	@LunaNordin
+# version:	0.1.1(08.11.2019)
+
 import time
 import threading
 
-FRONT = 0
-RIGHT = 1
-BACK = 2
-
-
 class SensorManager:
     """ controls the measured distances to the vehicle's front, right and back """
-
-    REFRESH_INTERVAL = 0.4
+		
+    REFRESH_INTERVAL = 0.5	#interval in which new values are calculated
 
     def __init__(self):
         """ initializes the sensor manager and starts the thread that refreshes the sensor data """
 
+		#start values for the three distances
         self.__distance_front = 0.0
         self.__distance_right = 0.0
         self.__distance_back = 0.0
 
-        refresh_thread = threading.Thread(target=self.refresh)
+        #setup a new thread to avoid using while True: 
+		refresh_thread = threading.Thread(target=self.refresh)
         refresh_thread.start()
 
     # TODO
@@ -26,7 +30,7 @@ class SensorManager:
         """ constantly updates the measured sensor data """
 
         while True:
-            time.sleep(self.REFRESH_INTERVAL)
+            time.sleep(self.REFRESH_INTERVAL)	#pause to keep timing in interval
 
     def get_distance(self, direction: int) -> float or None:
         """ gives the latest measured distance in a certain direction
@@ -38,7 +42,7 @@ class SensorManager:
                 float: latest measured distance if the direction can be interpreted
                 None: if the handed direction is not one of the directions defined at th top of this file
         """
-
+		#the input request is analyzed and the corresponding value is returned
         if direction == FRONT:
             return self.__distance_front
         elif direction == RIGHT:
