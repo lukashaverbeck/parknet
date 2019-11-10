6739f83e466faf667e350cbb65211196dcab6426
LukasGra <LukasGra@users.noreply.github.com>
2019-11-10 19:48:05 +0100

    Added comments

--- communication/Communication.py
+++ communication/Communication.py
@@ -3,30 +3,45 @@ import requests
 from projektkurs.communication.NetworkScan import NetworkScan
 
 
-def testhere(x):
-    print("arg was "+str(x))
-
 methods = []
 args = ["unset"]
 
 
 class Communication:
     def triggerEvent(topic, message):
+        """ This method triggers all events with the given topic and deliver the message to them
+
+            Args:
+                topic (String): topic for the event
+                message (String): message for the topic
+        """
+
         print ("Executing")
         for item in methods:
             print("Sending Message: " + message + " Topic: " + item["topic"])
             if item["topic"] == topic:
                 item['callback'](message)
     def subscribe(topic, callback):
+        """ This method allows to subscribe a method to a topic
+
+            Args:
+                topic (String): topic for the event
+                callback (Method): the method to run, when the event is triggered
+        """
         methods.append({'callback': callback, 'topic': topic})
     def send(topic , message):
+        """ This method sends a message under the given topic
+
+            Args:
+                topic (String): topic for the message
+                message (String): message to be delivered
+        """
         print (topic)
-        rt = requests.post("http://192.168.178.156", data={topic: message})
+        """rt = requests.post("http://192.168.178.156", data={topic: message})
+        """
         list = NetworkScan.scanIPsFromNetwork("e")
         for i in list:
             print(i)
             r = requests.post("http://" + i, data={topic: message})
-
-Communication.subscribe("t" ,testhere)
 
 
--- communication/NetworkScan.py
+++ communication/NetworkScan.py
@@ -4,12 +4,18 @@ from contextlib import closing
 class NetworkScan:
 
     def getLocalIP(unused):
+        """ This method returns the local IP of the current device
+        """
         IP1 = socketlib.gethostbyname(socketlib.gethostname())
         return IP1
 
 
 
     def check_if_up(ip_address):
+        """ This method returns if on a given ip adress a webserver is running
+            Args:
+                ip_address(String): ip4 adress from the local network
+        """
         socket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
         socket.settimeout(0.004)
         try:
@@ -21,6 +27,8 @@ class NetworkScan:
 
 
     def scanIPsFromNetwork(unused):
+        """ This method returns a list of used ips in the network
+        """
         list = []
         IP1 = NetworkScan.getLocalIP("t")
         IPsplit = IP1.split(".")
@@ -29,10 +37,7 @@ class NetworkScan:
             ip = IPNetwork + str(i)
             result = NetworkScan.check_if_up(ip)
             if result:
-                #print (str(result) + ip)
                 list.append(ip)
         if list.__contains__(IP1):
             list.remove(IP1)
         return list
-
-    #print(scanIPsFromNetwork("j"))
