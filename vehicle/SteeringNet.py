80b6467d79e67cc07f38528cca9ee3fd9404a7fe
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-11-11 15:59:52 +0100

    reduces model to angle predictions

--- vehicle/SteeringNet.py
+++ vehicle/SteeringNet.py
@@ -1,8 +1,5 @@
-# This file provides the code necessary for autonomous driving based on the vehicle's image input.
+# This file provides the code necessary for autonomous steering based on the vehicle's image input.
 # It includes the training, evaluating and predicting process for the according neural net.
-#
-# author: @lukashaverbeck
-# version: 1.1 (10.11.2019)
 #
 # TODO add functionality for setting chechpoints while training
 
@@ -10,7 +7,6 @@ import os
 import math
 import random
 import datetime
-import subprocess
 import numpy as np
 import tensorflow as tf
 from skimage.io import imread
@@ -20,8 +16,6 @@ class SteeringNet:
     """ covers the deep learning model by providing high level functionality for training, evaluating and predictions of
         a deep learning model for autonomous driving
     """
-
-    LOSS_WEIGHTS = [100.0, 0]
 
     def __init__(self):
         self.model = self.Model()
@@ -51,7 +45,7 @@ class SteeringNet:
 
         optimizer = tf.optimizers.Adam(lr)
         data_generator = generate_data(root_directory, csv_file_train, batch_size)
-        self.model.compile(loss=self.loss, loss_weights=self.LOSS_WEIGHTS, optimizer=optimizer, batch_size=batch_size)
+        self.model.compile(loss=self.loss, optimizer=optimizer, batch_size=batch_size)
         self.model.fit_generator(data_generator, steps_per_epoch=math.ceil(num_samples_train / batch_size), epochs=epochs, validation_data=validation_data, validation_steps=validation_steps, callbacks=[tensorboard_callback], verbose=verbose)
 
     def evaluate(self, num_samples, root_directory, csv_file, batch_size=64):
@@ -78,47 +72,38 @@ class SteeringNet:
                 batch_size (int): number of samples per batch
 
             Returns:
-                float, float: average deviations of the predicted angle and velocity from the real steering values
+                float: average deviation of the predicted steering angle from the real value
         """
 
         data_generator = generate_data(root_directory, csv_file, batch_size)
         steps = 0
         deviation_angle = 0.0
-        deviation_velocity = 0.0
 
         for inputs, target in data_generator:
-            prediction = self.model.predict(inputs, batch_size)
-            prediction_angle = prediction[0]
-            prediction_velocity = prediction[1]
-
+            prediction_angle = self.model.predict(inputs, batch_size)
             target_angle = target[0]
-            target_velocity = target[1]
 
             deviation_angle += np.mean(abs(abs(prediction_angle) - abs(target_angle)))
-            deviation_velocity += np.mean(abs(abs(prediction_velocity) - abs(target_velocity)))
             steps += 1
 
             if steps * batch_size >= num_samples:
                 break
 
         deviation_angle /= steps
-        deviation_velocity /= steps
 
         print("average deviation steering angle:", round(deviation_angle, 2))
-        print("average deviation velocity:", round(deviation_velocity, 2))
 
-        return deviation_angle, deviation_velocity
+        return deviation_angle
 
-    def predict(self, image_path, current_angle, current_velocity):
-        """ predicts a steering angle and a velocity based on the current image input and driving behaviour
+    def predict(self, image_path, current_angle):
+        """ predicts a steering angle based on the current image input and driving behaviour
 
             Args:
                 image_path (str): absolute path to the image
                 current_angle (float): the vehicle's current steering angle
-                current_velocity (float): the vehicle's current velocity
 
             Returns:
-                float, float: predicted steering angle and predicted velocity
+                float: predicted steering angle
         """
 
         image = imread(image_path)
@@ -128,9 +113,9 @@ class SteeringNet:
         current_angle = np.array([current_angle])
 
         inputs = {"image": image, "old_angle": current_angle}
-        prediction = self.model.predict(inputs)[0]
+        angle = self.model.predict(inputs)[0][0]
         
-        return prediction[0]
+        return angle
 
     def save(self, model_name):
         """ saves the weights of the model to disk
@@ -162,51 +147,48 @@ class SteeringNet:
             super().__init__()
 
             self.conv1 = tf.keras.layers.Conv2D(3, (5, 5), activation="elu")
-            self.conv1 = tf.keras.layers.Conv2D(16, (5, 5), activation="elu")
-            self.conv2 = tf.keras.layers.Conv2D(32, (3, 3), activation="elu")
+            self.conv1 = tf.keras.layers.Conv2D(24, (5, 5), activation="elu")
+            self.conv2 = tf.keras.layers.Conv2D(36, (5, 5), activation="elu")
             self.conv3 = tf.keras.layers.Conv2D(48, (3, 3), activation="elu")
+            self.conv4 = tf.keras.layers.Conv2D(64, (3, 3), activation="elu")
+            self.conv5 = tf.keras.layers.Conv2D(64, (3, 3), activation="elu")
+            self.pool1 = tf.keras.layers.MaxPool2D((2, 2))
 
-            self.pool1 = tf.keras.layers.MaxPool2D((3, 3))
             self.flat1 = tf.keras.layers.Flatten()
-            self.drop1 = tf.keras.layers.Dropout(0.3)
+            self.flcn1 = tf.keras.layers.Dense(100, activation="elu")
+            self.flcn2 = tf.keras.layers.Dense(50, activation="elu")
+            self.flcn3 = tf.keras.layers.Dense(10, activation="elu")
+            self.flcn4 = tf.keras.layers.Dense(1)
 
-            self.flcn1 = tf.keras.layers.Dense(1024, activation="elu")
-            self.flcn2 = tf.keras.layers.Dense(512, activation="elu")
-            self.flcn3 = tf.keras.layers.Dense(256, activation="elu")
-            self.flcn4 = tf.keras.layers.Dense(128, activation="elu")
-            self.flcn5 = tf.keras.layers.Dense(64)
-            self.flcn6 = tf.keras.layers.Dense(2)
-
-            self.convolutional_layers = [self.conv1, self.conv2, self.conv3]
-            self.dense_layers = [self.flcn1, self.flcn2, self.flcn3, self.flcn4, self.flcn5, self.flcn6]
+            self.dense_layers = [self.flcn1, self.flcn2, self.flcn3, self.flcn4]
 
         def call(self, inputs):
             """ propagates forward through the neural net
 
                 Args:
-                    inputs (dict): list of numpy arrays containing the input batches (images, angles, velocities)
+                    inputs (dict): list of numpy arrays containing the input batches (images, angles)
             """
 
             x = inputs["image"]
             old_angle = inputs["old_angle"]
-            old_velocity = inputs["old_velocity"]
 
-            for convolution in self.convolutional_layers:
-                x = convolution(x)
-                x = self.pool1(x)
-                x = self.drop1(x)
+            x = self.conv1(x)
+            x = self.pool1(x)
+            x = self.conv2(x)
+            x = self.pool1(x)
+            x = self.conv3(x)
+            x = self.pool1(x)
+            x = self.conv4(x)
+            x = self.pool1(x)
+            x = self.conv5(x)
 
             x = self.flat1(x)
-            x = tf.concat([x, old_angle, old_velocity], 1)
+            x = tf.concat([x, old_angle], 1)
 
             for dense in self.dense_layers:
-                x = self.drop1(x)
                 x = dense(x)
 
-            angles = x[:, 0]
-            velocities = x[:, 1]
-
-            return angles, velocities
+            return x
 
 
 def generate_data(root_directory, csv_file, batch_size):
@@ -219,17 +201,15 @@ def generate_data(root_directory, csv_file, batch_size):
             batch_size (int): number of samples per batch
 
         Yields:
-            dict, np.array: dictionary of batches with arrays for the images, the old steering angle, the old
-                            velocity as well as an label with the according steering angle and velocity
+            dict, np.array: dictionary of batches with arrays for the images, the old steering angle as well
+                            as an label with the according steering angle and velocity
 
         TODO substitute randomly generated old steering values by real angles
     """
 
     batch_image = []
     batch_angle = []
-    batch_velocity = []
     batch_old_angle = []
-    batch_old_velocity = []
 
     while True:
         with open(csv_file) as log:
@@ -251,22 +231,15 @@ def generate_data(root_directory, csv_file, batch_size):
                 old_angle = angle * random.uniform(0.90, 1.1)
                 batch_angle.append(angle)
 
-                velocity = float(cells[4])
-                old_velocity = velocity * random.uniform(0.90, 1.1)
-                batch_velocity.append(velocity)
-
                 batch_image.append(image)
                 batch_old_angle.append(old_angle)
-                batch_old_velocity.append(old_velocity)
 
                 if len(batch_image) == batch_size:
-                    inputs = {"image": np.array(batch_image), "old_angle": np.array(batch_old_angle), "old_velocity": np.array(batch_old_velocity)}
-                    target = [np.array(batch_angle), np.array(batch_velocity)]
+                    inputs = {"image": np.array(batch_image), "old_angle": np.array(batch_old_angle)}
+                    target = np.array(batch_angle)
 
                     yield (inputs, target)
 
                     batch_image = []
                     batch_angle = []
-                    batch_velocity = []
                     batch_old_angle = []
-                    batch_old_velocity = []
