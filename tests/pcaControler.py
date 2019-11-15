a461e81fab9598f841d999f73b6236aa8fceac73
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-11-15 21:56:59 +0100

    Create pcaControler.py
    Control individual PCA9685 channels.

---
+++ tests/pcaControler.py
@@ -0,0 +1,6 @@
+#Use this script to control any type of pwm-controlled motor attached to the PCA9685 module.
+#It allows you to send a specific pwm signal to the seperate channels of the PCA9685 module as well
+#as to one of several modules by specifying a I2C-bus.
+
+#author:   @LunaNordin
+#version:  1.0(15.11.2019)
