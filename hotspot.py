4a3067f809297ab4cf8eda68867e09dec2f2bc01
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-11-16 14:08:21 +0100

    fixed test

--- hotspot.py
+++ hotspot.py
@@ -1,25 +1,14 @@
-#Script opens up a new accesspoint.
+# test for opening up a new accesspoint.
 
-#author:	@LukasGra
-#author:	@LunaNordin
-#version:	0.1(09.11.2019)
-
-from PyAccessPoint import pyaccesspoint	#library basis to create an accesspoint
 import time
-
-access_point = pyaccesspoint.AccessPoint()	#crate an accespoint-object
-
-access_point.start()	#open up the accesspoint
-
-while True:
-	time.sleep(5)
-	if access_point.is_running() == True:	#check for accespoint still being active
-  		print("WLAN ist an")	
-	elif access_point.is_running == False:
-		print("WLAN ist aus")
+import threading
+from PyAccessPoint import pyaccesspoint
 
 
-##access_point.stop()
+access_point = pyaccesspoint.AccessPoint(ip="192.168.2.116")
+thread = threading.Thread(target=access_point.start)
 
-#AccessPoint:
-#  def __init__(self, wlan='wlan0', inet=None, ip='192.168.45.1', netmask='255.255.255.0', ssid='MyAccessPoint', password='1234567890'):
+access_point.stop()
+thread.start()
+time.sleep(30)
+access_point.stop()
