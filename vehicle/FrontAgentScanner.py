3da0aa8b6c126d36b4b78ca37a70e19adce577c1
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-11-19 15:36:15 +0100

    embedded pi camera

--- vehicle/FrontAgentScanner.py
+++ vehicle/FrontAgentScanner.py
@@ -1,19 +1,18 @@
-import cv2
 import time
 import threading
 import pyzbar.pyzbar as pyzbar
+import picamera
+import picamera.array
 
 
 class FrontAgentScanner:
 
-    REFRESH_INTERVAL = 2
+    REFRESH_INTERVAL = 1
 
     def __init__(self):
         """ initializes the scanner and starts to refresh the front agent ID """
 
         self.__front_agent_id = None
-        # TODO Pi-Kamera initialisieren statt Webkamera
-        self.__camera = cv2.VideoCapture(0)
 
         refresh_thread = threading.Thread(target=self.refresh)
         refresh_thread.start()
@@ -21,17 +20,24 @@ class FrontAgentScanner:
     def refresh(self):
         """ permanently updates the front agent ID based on the current video input """
 
-        while True:
-            _, frame = self.__camera.read()
-            decoded_objects = pyzbar.decode(frame)
+        with picamera.PiCamera() as camera:
+            while True:
+                with picamera.array.PiRGBArray(camera) as frame:
+                    camera.capture(frame, "rgb")
+                    decoded_objects = pyzbar.decode(frame.array)
 
-            if len(decoded_objects) > 0:
-                data_bytes = decoded_objects[0].data
-                self.__front_agent_id = data_bytes.decode("utf-8")
-            else:
-                self.__front_agent_id = None
+                    if len(decoded_objects) > 0:
+                        data_bytes = decoded_objects[0].data
+                        self.__front_agent_id = data_bytes.decode("utf-8")
+                    else:
+                        self.__front_agent_id = None
 
-            time.sleep(self.REFRESH_INTERVAL)
+                    print(self.__front_agent_id)
+
+                    time.sleep(self.REFRESH_INTERVAL)
 
     def get_front_agent_id(self):
         return self.__front_agent_id
+
+
+FrontAgentScanner()
