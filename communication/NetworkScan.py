6da3bf52ac92e4dbcc139bbbe9ff72cd879359ea
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-11-19 20:16:53 +0100

    restrucutured code

--- communication/NetworkScan.py
+++ communication/NetworkScan.py
@@ -1,43 +1,52 @@
 import socket as socketlib
 from contextlib import closing
 
-class NetworkScan:
+def get_local_ip(self):
+    """ determines the local IP of the current device
 
-    def getLocalIP(unused):
-        """ This method returns the local IP of the current device
-        """
-        IP1 = socketlib.gethostbyname(socketlib.gethostname())
-        return IP1
+        Returns:
+            str: local IP of the current device
+    """
 
+    ip = socketlib.gethostbyname(socketlib.gethostname())
+    return ip
 
+def check_if_up(self, ip_address):
+    """ This method returns if on a given ip adress a webserver is running
+        
+        Args:
+            ip_address (str): ip4 adress from the local network
+    """
 
-    def check_if_up(ip_address):
-        """ This method returns if on a given ip adress a webserver is running
-            Args:
-                ip_address(String): ip4 adress from the local network
-        """
-        socket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
-        socket.settimeout(0.004)
-        try:
-            with closing(socket):
-                socket.connect((str(ip_address), 80))
-                return True
-        except socketlib.error:
-            return False
+    socket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
+    socket.settimeout(0.004)
+    
+    try:
+        with closing(socket):
+            socket.connect((str(ip_address), 80))
+            return True
+    except socketlib.error:
+        return False
 
+def scan_ips_from_network(self):
+    """ This method returns a list of used ips in the network
 
-    def scanIPsFromNetwork(unused):
-        """ This method returns a list of used ips in the network
-        """
-        list = []
-        IP1 = NetworkScan.getLocalIP("t")
-        IPsplit = IP1.split(".")
-        IPNetwork = IPsplit[0] + "." + IPsplit[1] + "." + IPsplit[2] + "."
-        for i in range(1, 158):
-            ip = IPNetwork + str(i)
-            result = NetworkScan.check_if_up(ip)
-            if result:
-                list.append(ip)
-        if list.__contains__(IP1):
-            list.remove(IP1)
-        return list
+        Returns:
+            list: list of used ips in the network
+    """
+
+    ips = []
+    local_ip = get_local_ip("t")
+    ip_parts = local_ip.split(".")
+    ip_network = ip_parts[0] + "." + ip_parts[1] + "." + ip_parts[2] + "."
+
+    for i in range(1, 158):
+        ip = ip_network + str(i)
+        result = check_if_up(ip)
+        if result:
+            ips.append(ip)
+
+    if local_ip in ips:
+        ips.remove(local_ip)
+    
+    return ips
