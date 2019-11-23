ac40cc794e265655d7e30cdf5e5f2f2de4f2b8f6
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-11-23 16:22:50 +0100

    Update hotspot.py
    network information still needs to be updated

--- tests/hotspot.py
+++ tests/hotspot.py
@@ -1,14 +1,20 @@
-# test for opening up a new accesspoint.
+#Opens up a standalone Wifi Network with user determined network information
+#author:  @LukasGra
+#author:  @lukashaverbeck
+#author:  @LunaNordin
+#version:  1.0(23.11.2019)
 
 import time
 import threading
-from PyAccessPoint import pyaccesspoint
+from PyAccessPoint import pyaccesspoint  #PiAccesPoint library
 
 
-access_point = pyaccesspoint.AccessPoint(ip="192.168.2.116")
-thread = threading.Thread(target=access_point.start)
+access_point = pyaccesspoint.AccessPoint(ip="192.168.2.116")  #create AccessPoint-object with determined ip
+thread = threading.Thread(target=access_point.start)  #start a thread on running network
 
-access_point.stop()
-thread.start()
-time.sleep(30)
+print("Creating access point at port: 22 with IP-address: 192.168.2.116 and passwort: 1234567890")
+print("In order to connect to your picar via SSH use network information and standart user information for login.")
+
+access_point.stop()  #stop potential legacy acces points
+thread.start()  #start thread
 access_point.stop()
