e519e4f1b4873a979455852e887cd9dec6e39042
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-12-01 22:07:24 +0100

    now valid for positive and negative angles

--- tests/angle.py
+++ tests/angle.py
@@ -1,28 +1,31 @@
 #Script gets user input from commandline and sets this as pwm value.
-#Calculates angle from pwm with math function.
+#Calculates pwm value from angle with math function.
 
-#author:	@Lunanordin
-#version:	1.1(26.11.2019)
+#author:  @Lunanordin
+#version:  2.0(02.12.2019)
 
 
 from __future__ import division
 import time
-import math
+import math	#math is used to define function
 import Adafruit_PCA9685  #import of library for PCA9685-module
 
 pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)  #create PCA9685-object at I2C-port
 pwm.set_pwm_freq(50)  #set frequency
 
 def calc_angle(x):
- val = 0.002895*(math.pow(x,2))-2.37867*x+466.65
+ '''gets angle and calculates coresponding pwm value'''
+ val = 0.000002*(math.pow(x,4))+0.000002*(math.pow(x,3))+0.005766*(math.pow(x,2))-(1.81281*x)+324.149
  return val
 
 while True:
- pwm_val = input("Please enter your pwm value: ")  #get user input from commandline (as string)
+ pwm_val = input("Please enter your angle: ")  #get user input from commandline (as string)
  
- if pwm_val == "q":
+ if pwm_val == "q":	#if user types "q" end the program
    break
  
- pwm.set_pwm(0, 0, int(pwm_val))  #cast user input to integer and set pwm value
- print("Angle is: " + str(calc_angle(int(pwm_val))) + "")  #Echo user input
+ pwm_calc = calc_angle(int(pwm_val))	#a pwm vale is calculated from user input
+ 
+ pwm.set_pwm(0, 0, int(pwm_calc))  #cast pwm value to integer and set it on pwm object
+ print("pwm value is: " + str(pwm_calc) + "")  #Echo user input
  
