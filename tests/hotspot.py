4e66dcc13ac30e0d75dcaec804f8630a4093bc0d
LukasGra <LukasGra@users.noreply.github.com>
2019-12-01 20:28:21 +0100

    Updated settings custom ssid and password

--- tests/hotspot.py
+++ tests/hotspot.py
@@ -1,20 +1,34 @@
 #Opens up a standalone Wifi network with user determined network information
-#author:  @LukasGra
-#author:  @lukashaverbeck
-#author:  @LunaNordin
 #version:  1.0(23.11.2019)
 
 import time
 import threading
+import time
+import logging
 from PyAccessPoint import pyaccesspoint  #PyAccesPoint library
 
 
-access_point = pyaccesspoint.AccessPoint(ip="192.168.1.001")  #create AccessPoint-object with determined ip
-thread = threading.Thread(target=access_point.start)  #start a thread on running network
+logging.basicConfig(format="%(asctime)s ::%(levelname)s:: %(message)s",
+                        level=logging.DEBUG)
+
+access_point = pyaccesspoint.AccessPoint(ssid="Test123" , password="Hallo123")  #create AccessPoint-object with determined ip
+access_point.start()  #start a thread on running network
+
+print (access_point.is_running());
 
 print("Creating access point at port: 22 with IP-address: 192.168.1.001 and passwort: 1234567890")
 print("In order to connect to your picar via SSH use network information and standard user information for login.")
 
+
+time.sleep(30)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+print ("Shutting down!")
 access_point.stop()  #stop potential legacy acces points
-thread.start()  #start thread
+#thread.start()  #start thread
 access_point.stop()
