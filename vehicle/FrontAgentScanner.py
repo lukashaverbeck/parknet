0ed56b46a4698a392a9198b36500f204f6a86508
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-12-06 16:50:38 +0100

    debugged code, camera now working

--- vehicle/FrontAgentScanner.py
+++ vehicle/FrontAgentScanner.py
@@ -15,7 +15,7 @@ class FrontAgentScanner:
         """ initializes the scanner and starts to refresh the front agent ID """
 
         self.__front_agent_id = None
-        self.__camera = Camera()
+        self.__camera = Camera.instance()
 
         # start updating the front agent ID in a separate thread
         refresh_thread = threading.Thread(target=self.refresh)
@@ -28,13 +28,16 @@ class FrontAgentScanner:
 
         while True:
             # analyze the camera input with respect to QR Codes representing an agent ID
-            decoded_objects = pyzbar.decode(self.__camera.get_image())
+            img_array = self.__camera.get_image()
+            
+            if img_array is not None:
+                decoded_objects = pyzbar.decode(img_array)
 
-            if len(decoded_objects) > 0:
-                data_bytes = decoded_objects[0].data
-                self.__front_agent_id = data_bytes.decode("utf-8")
-            else:
-                self.__front_agent_id = None
+                if len(decoded_objects) > 0:
+                    data_bytes = decoded_objects[0].data
+                    self.__front_agent_id = data_bytes.decode("utf-8")
+                else:
+                    self.__front_agent_id = None
 
             time.sleep(self.REFRESH_INTERVAL)
 
