003c50eb3ac9a58c8692b1f1f8a57b62c2ade265
LukasGra <LukasGra@users.noreply.github.com>
2019-12-07 20:44:28 +0100

    Added new test classes testCommunication, wificonnector and wifiscan

---
+++ tests/testCommunication.py
@@ -0,0 +1,14 @@
+from projektkurs.vehicle.Communication import Communication
+import requests
+
+
+def test(hallo):
+    print ("e" + str(hallo))
+    #com.send("Halk" , "t" , 1)
+    #rt = requests.post("http://" + "192.168.178.156", data={"e": "e"})
+    #rt = requests.get("http://192.168.178.156")
+
+com = Communication(1)
+com.subscribe("Hallo" , test)
+com.send("Hallo", "test",1)
+  # only for test purposes
