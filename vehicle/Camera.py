15dd91dafa6130a2de084b6aa294e71c4a6a291a
lukashaverbeck <lukas.haverbeck@gmail.com>
2019-12-09 16:05:33 +0100

    moved `Singleton` to util module

--- vehicle/Camera.py
+++ vehicle/Camera.py
@@ -3,6 +3,7 @@ import threading
 import picamera
 import picamera.array
 from PIL import Image
+from util import Singleton
 
 
 def save_img_array(img_array, path):
@@ -23,33 +24,6 @@ def save_img_array(img_array, path):
     img.save(path)
 
     return True
-
-
-class Singleton:
-    """ helper class ensuring that classes decorated with it can only be instantiated one single time """
-
-    def __init__(self, decorated):
-        self.__instance = None
-        self.__decorated = decorated
-
-    def instance(self):
-        """ gets the same instance of a particluar class every time it is called
-
-            returns:
-                object: any object decorated with `Singleton` but the same instance of it every time
-        """
-
-        if self.__instance is None:
-            self.__instance = self.__decorated()
-            return self.__instance
-        
-        return self.__instance
-
-    def __call__(self):
-        raise TypeError("Singletons must be accessed through `instance()`.")
-
-    def __instancecheck__(self, instance):
-        return isinstance(instance, self.__decorated)
 
 
 @Singleton
