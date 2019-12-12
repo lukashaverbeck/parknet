f0f455443962e20b8296e561ce3ef1d9544a5491
LukasGra <LukasGra@users.noreply.github.com>
2019-12-12 09:45:47 +0100

    Bugfixes for wlan blocking

--- tests/AutoConnector.py
+++ tests/AutoConnector.py
@@ -18,6 +18,7 @@ class ssid_block_object():
     def get_block_time(self):
         return self.blocktime
 
+
 class AutoConnector(Thread):
 
     def __init__(self, event):
@@ -25,29 +26,38 @@ class AutoConnector(Thread):
         self.stopped = event
         self.count = 0
         self.wlan_count_found = 0
-        self.hotspot_status = False
         self.wireless_module = Wireless()
+
         self.wlan_name_found_to_connect = ""
         self.wlan_name_found_to_connect_time = 999999999999
         self.wlan_default_password = "Hallo1234"
-        self.own_wlan_name_from_hotspot = "parknet"
-        self.own_wlan_time_from_hotspot = 9999999999999
-        self.last_wlan_connected = "unset"
-        self.block_list = []
+
         self.access_point = pyaccesspoint.AccessPoint(ssid="Test Wlan", password=self.wlan_default_password)
         logging.basicConfig(format="%(asctime)s ::%(levelname)s:: %(message)s", level=logging.DEBUG)  # Enable Debug
 
         self.access_point.stop()
 
+        self.hotspot_status = False
+        self.own_wlan_name_from_hotspot = "parknet"
+        self.own_wlan_time_from_hotspot = 9999999999999
+
+
+
+        self.last_wlan_connected = "unset"
+        self.block_list = []
+
     def run(self):
         while not self.stopped.wait(5):
             self.wlan_count_found = 0
-            self.count = self.count + 1
-            print("Connected with: " + self.wireless_module.current())
+            self.count += 1
+            print("+++++++++++++++Connected with: " + str(self.wireless_module.current()))
             if self.wireless_module.current() is None:
                 if self.last_wlan_connected != "unset":
-                    self.add_to_block_list(self.last_wlan_connected, int(time.time()) + 60)
+                    time_for_block = int(time.time()) + 60
+                    self.add_to_block_list(self.last_wlan_connected, time_for_block)
                     self.last_wlan_connected = "unset"
+            else:
+                self.last_wlan_connected = self.wireless_module.current()
             self.wlanscan()
             print("Loop Count: " + str(self.count) + " Wlans: " + str(self.wlan_count_found))
             self.connect_to_Network_or_create_hotspot()
@@ -64,9 +74,10 @@ class AutoConnector(Thread):
                         try:
                             time_wlan_create = int(wlanscan.ssid.replace("parknet", ""))
                             if time_wlan_create < self.wlan_name_found_to_connect_time:
-                                if not self.is_blocked(wlanscan.ssid):
-                                    self.wlan_count_found = self.wlan_count_found + 1
-                                    print(str(wlanscan.ssid) + " Quality: " + str(wlanscan.quality) + " Protected: " + str(
+                                if not self.is_blocked(str(wlanscan.ssid)):
+                                    self.wlan_count_found += 1
+                                    print(str(wlanscan.ssid) + " Quality: " + str(
+                                        wlanscan.quality) + " Protected: " + str(
                                         wlanscan.encrypted))
                                     self.wlan_name_found_to_connect = wlanscan.ssid
                                     self.wlan_name_found_to_connect_time = time_wlan_create
@@ -81,6 +92,7 @@ class AutoConnector(Thread):
                 if wlanscan.ssid == "Lukas123":
                     self.wlan_name_found_to_connect = wlanscan.ssid
                     self.wlan_name_found_to_connect_time = 1
+                    self.wlan_count_found += 1
                     print("Found Lukas Wlan!!!11")
                     break
         except:
@@ -88,6 +100,7 @@ class AutoConnector(Thread):
 
     def start_hotspot(self):
         if not self.hotspot_status:
+            print("Starting hotspot...")
             logging.basicConfig(format="%(asctime)s ::%(levelname)s:: %(message)s",
                                 level=logging.DEBUG)  # Enable Debug
             self.own_wlan_name_from_hotspot = "parknet" + str(int(time.time()))
@@ -99,6 +112,7 @@ class AutoConnector(Thread):
 
     def stop_hotspot(self):
         if self.hotspot_status:
+            print("Disabling hotspot...")
             logging.basicConfig(format="%(asctime)s ::%(levelname)s:: %(message)s",
                                 level=logging.DEBUG)  # Enable Debug
 
@@ -108,21 +122,25 @@ class AutoConnector(Thread):
 
     def connect_to_Network_or_create_hotspot(self):
         if self.wlan_count_found <= 0:
-            print("Starting hotspot...")
+            print("Hotspot mode")
             self.start_hotspot()
         else:
             if self.own_wlan_time_from_hotspot > self.wlan_name_found_to_connect_time:
                 if self.hotspot_status:
-                    print("Disabling hotspot...")
+                    print("Hotspot mode off")
                     self.stop_hotspot()
                 else:
-                    print("Wlan connected: " + str(self.wireless_module.current()))
+                    # print("Wlan connected: " + str(self.wireless_module.current()))
                     if self.wireless_module.current() != self.wlan_name_found_to_connect:
-                        print("Connecting to " + str(self.wlan_name_found_to_connect))
+                        print("Connecting to network " + str(self.wlan_name_found_to_connect)+ "...")
                         print("Status: " + str(
                             self.wireless_module.connect(self.wlan_name_found_to_connect, self.wlan_default_password)))
+                        time.sleep(2)
+                        print("Wlan network: " + str(self.wireless_module.current()))
+                        if self.wireless_module.current() is not None:
+                            self.last_wlan_connected = self.wireless_module.current()
 
-    def is_blocked(self,ssid):
+    def is_blocked(self, ssid):
         for block_obj in self.block_list:
             if block_obj.get_ssid == ssid:
                 if block_obj.get_block_time() > int(time.time()):
@@ -131,9 +149,9 @@ class AutoConnector(Thread):
                     return False
         return False
 
-    def add_to_block_list(self,ssid, blocktime):
+    def add_to_block_list(self, ssid, blocktime):
         self.block_list.append(ssid_block_object(ssid, blocktime))
-        print ("--------------------Blocking " + ssid )
+        print("--------------------Blocking " + ssid + " for " + str(blocktime))
 
     def print_list(self):
         for block_obj in self.block_list:
