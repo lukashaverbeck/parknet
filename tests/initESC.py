f82dea84867515b758989330f57dfd4c28241033
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-12-15 19:23:42 +0100

    now setting digital zero treble point

--- tests/initESC.py
+++ tests/initESC.py
@@ -9,8 +9,7 @@ import Adafruit_PCA9685  # Import the PCA9685 module
 
 pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)  # assign I2C-address and busnumber
 pwm.set_pwm_freq(50)  # setting frequency
-
-init_pwm = 340  #digital treble zero position
+init_pwm = 340  # digital treble zero position
 
 print("Please make sure to disconnect your ESC from the battery.")
 input = input("Press Enter to start initialization process: ")
@@ -19,4 +18,4 @@ print("\nTreble-Zero pulse is send to ESC-port...", end ="")
 pwm.set_pwm(0, 0, init_pwm)  # sending activation frequency
 print("done!")
 print("Now power on your ESC and wait until you hear it beeping.")
-time.sleep(15)  #time to turn on ESC 
+time.sleep(15)  # time to turn on ESC
