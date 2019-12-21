1e698e7e31261c63d5471d5b0d2ef859da613077
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-12-21 17:21:45 +0100

    finished version 1.0

--- tests/stepper.py
+++ tests/stepper.py
@@ -1,20 +1,20 @@
-# drives a bi-polar stepper motor
+# drives a bi-polar stepper motor one clockwise round
 # author:   @LunaNordin
 # version:  1.0(21.12.2019)
 
-from time import sleep
-import RPi.GPIO as GPIO
+from time import sleep  # sleep method used for timing of steps
+import RPi.GPIO as GPIO  # RPi.GPIO for GPIO-pin control
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setwarnings(False)  # disable GPIO warnings for console convenience
+GPIO.setmode(GPIO.BCM)  # GPIO instance works in broadcom-memory mode
+GPIO.setup(20, GPIO.OUT)  # direction pin is an output
+GPIO.setup(21, GPIO.OUT)  # step pin is an output
 
-GPIO.output(20, 1)
+GPIO.output(20, 1)  # direction is set to clockwise
 
-delay = 0.005
-for i in range(200):
-    GPIO.output(21, GPIO.HIGH)
+delay = 0.005   # delay based on step count
+for i in range(200):  # loop turns stepper one round in two seconds
+    GPIO.output(21, GPIO.HIGH)  # make on step
     sleep(delay)
-    GPIO.output(21, GPIO.LOW)
+    GPIO.output(21, GPIO.LOW)  # turn step pin low to prepare next step
     sleep(delay)
