bc2fe88af17b090f2d0c90bed95ab2817eb4630a
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2019-12-22 17:00:39 +0100

    ver2.0 includes parametrs based on user input

--- tests/stepper.py
+++ tests/stepper.py
@@ -10,11 +10,21 @@ GPIO.setmode(GPIO.BCM)  # GPIO instance works in broadcom-memory mode
 GPIO.setup(20, GPIO.OUT)  # direction pin is an output
 GPIO.setup(21, GPIO.OUT)  # step pin is an output
 
-GPIO.output(20, 1)  # direction is set to clockwise
+direction = input("Type 'c' for clockwork and 'r' for counterclockwise: ")
+if direction == "c":
+    GPIO.output(20, 1)  # direction is set to clockwise
+elif direction == "r":
+    GPIO.output(20, 0)  # direction is set to counterclockwise
 
-delay = 0.005   # delay based on step count
-for i in range(200):  # loop turns stepper one round in two seconds
+rps = input("Please type in your rps: ")
+delay = (1/(200*int(rps)))/2   # delay based on rounds per second
+
+rotations = input("Type in your number of rotations: ")
+steps = int(rotations)*200  # steps calculated from rotation number
+
+for i in range(steps):  # loop turns stepper one round in two seconds
     GPIO.output(21, GPIO.HIGH)  # make on step
     sleep(delay)
     GPIO.output(21, GPIO.LOW)  # turn step pin low to prepare next step
     sleep(delay)
+    
