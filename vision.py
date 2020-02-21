a8b6b3be411ed05d950a88cab427b5a2037c35f3
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2020-02-21 21:43:51 +0100

    changed data selection method
    the distance measurement now only is done if requested by another class to avoid interference with the stepper motor GPIO layout and function

--- vision.py
+++ vision.py
@@ -11,7 +11,7 @@ import time
 from threading import Thread
 import picamera
 import picamera.array
-import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
 import pyzbar.pyzbar as pyzbar
 import constants as const
 from util import Singleton, threaded
@@ -26,75 +26,64 @@ class SensorManager:
     def __init__(self):
         """ initializes the sensor manager and starts the thread that refreshes the sensor data """
 
-        self.front = 0.0
-        self.right = 0.0
-        self.rear = 0.0
-        self.rear_angled = 0.0
-        
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        self.sensor_front = DistanceSensor(echo=const.EchoPin.FRONT, trigger=const.TriggerPin.FRONT)
+        self.sensor_right = DistanceSensor(echo=const.EchoPin.RIGHT, trigger=const.TriggerPin.RIGHT)
+        self.sensor_back = DistanceSensor(echo=const.EchoPin.BACK, trigger=const.TriggerPin.BACK)
+        self.sensor_back_angled = DistanceSensor(echo=const.EchoPin.BACK_ANGLED, trigger=const.TriggerPin.BACK_ANGLED)
 
-        # define all trigger pins as outputs and all echo pins as inputs
-        # make sure all pins ar free to use to avoid data collision
-        GPIO.setup(const.TriggerPin.FRONT, GPIO.OUT)
-        GPIO.setup(const.TriggerPin.RIGHT, GPIO.OUT)
-        GPIO.setup(const.TriggerPin.BACK, GPIO.OUT)
-        GPIO.setup(const.TriggerPin.BACK_ANGLED, GPIO.OUT)
-        GPIO.setup(const.EchoPin.FRONT, GPIO.IN)
-        GPIO.setup(const.EchoPin.RIGHT, GPIO.IN)
-        GPIO.setup(const.EchoPin.BACK, GPIO.IN)
-        GPIO.setup(const.EchoPin.BACK_ANGLED, GPIO.IN)
+        self.updates = {
+            "front": {
+                "time": 0,
+                "value": 0
+            },
+            "right": {
+                "time": 0,
+                "value": 0
+            },
+            "rear": {
+                "time": 0,
+                "value": 0
+            },
+            "rear_angled": {
+                "time": 0,
+                "value": 0
+            }
+        }
 
-        # pull down trigger pins in case the pin is still activated
-        GPIO.output(const.TriggerPin.FRONT, False)
-        GPIO.output(const.TriggerPin.RIGHT, False)
-        GPIO.output(const.TriggerPin.BACK, False)
-        GPIO.output(const.TriggerPin.BACK_ANGLED, False)
-        time.sleep(2)  # wait two seconds to make sure there are no signal fragments which could be detected
+        front = self.sensor_front.distance * 100
+        print(front)
 
-        # start updating the distances in a separate thread
-        self.update()
+    @property
+    def front(self):
+        direction = "front"
+        if self.updates[direction]["time"] < time.time()-self.REFRESH_INTERVAL:
+            self.updates[direction]["value"] = self.sensor_front.distance * 100
+            self.updates[direction]["time"] = time.time()
+        return self.updates[direction]["value"]   
 
-    @threaded
-    def update(self):
-        """ constantly updates the measured distances """
+    @property
+    def right(self):
+        direction = "right"
+        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
+            self.updates[direction]["value"] = self.sensor_right.distance * 100
+            self.updates[direction]["time"] = time.time()
+        return self.updates[direction]["value"]
 
-        while True:
-            # calculate and save distances
-            self.front = self.calc_distance(const.TriggerPin.FRONT, const.EchoPin.FRONT)
-            self.right = self.calc_distance(const.TriggerPin.RIGHT, const.EchoPin.RIGHT)
-            self.rear = self.calc_distance(const.TriggerPin.BACK, const.EchoPin.BACK)
-            #self.rear_angled = self.calc_distance(const.TriggerPin.BACK_ANGLED, const.EchoPin.BACK_ANGLED)
-            
-            time.sleep(self.REFRESH_INTERVAL)
+    @property
+    def rear(self):
+        direction = "rear"
+        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
+            self.updates[direction]["value"] = self.sensor_back.distance * 100
+            self.updates[direction]["time"] = time.time()
+        return self.updates[direction]["value"]
 
-    def calc_distance(self, trig_pin, echo_pin):
-        """ triggers the module at given pins and calculates the measured distance
-
-            Args:
-                trig_pin (int): ID of the pin to send a signal
-                echo_pin (int): ID of the pin to receive a signal
-                NOTE these arguments should be passed as the according constants -> `constants.py`
-
-            Returns:
-                float: the measured distance in cm rounded to two decimal places
-        """
-
-        GPIO.output(trig_pin, True)  # activate the trigger channel of HC-SR04 module
-        time.sleep(0.00001)  # 10μs pulse activates 8 ultrasound bursts at 40 kHz
-        GPIO.output(trig_pin, False)  # deactivate trigger channel
-
-        # measure time in which echo signal is detected
-        while GPIO.input(echo_pin) == 0:
-            pulse_start = time.time()
-
-        # measure the time of the echo signal
-        while GPIO.input(echo_pin) == 1:
-            pulse_end = time.time()
-
-        pulse_duration = pulse_end - pulse_start  # time it took the signal to hit the objectand return to sensor
-        distance = pulse_duration * 17150  # calculate into cm 34300[cm/s] = Distance[cm] / (Time[s]/2)
-        return round(distance, 2)
+    @property
+    def rear_angled(self):
+        direction = "rear_angled"
+        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
+            self.updates[direction]["value"] = self.sensor_back_angled.distance * 100
+            self.updates[direction]["time"] = time.time()
+        return self.updates[direction]["value"]
 
 
 @Singleton
