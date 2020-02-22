60c8705e416afb5fa7ca71617f1228d9eb438975
lukashaverbeck <lukas.haverbeck@gmail.com>
2020-02-22 09:04:24 +0100

    optimized and documented lazy sensor updates

--- vision.py
+++ vision.py
@@ -17,79 +17,152 @@ import constants as const
 from util import Singleton, threaded
 
 
+def lazy_sensor_update(direction, sensor):
+    """ decorator function that ensures that a distance sensor is only activated in certain time intervals
+        NOTE this decorator should only be used as a decorator for distance sensor data property functions
+
+        Args:
+            direction (str): direction of the distance sensor
+            sensor (DistanceSensor): sensor capturing distance data
+
+        Returns:
+            function: decorator wrapper function
+    """
+
+    def wrapper(func):
+        """ wrapper function containing the update function
+
+            Args:
+                func (function): property function for a specific sensor
+
+            Returns:
+                function: decorator function updating the sensor data
+        """
+
+        def update(sensor_manager):
+            """ function that updated a sensor data point if it is being requested and has not been updated for a while
+
+                Args:
+                    sensor_manager (SensorManager): instance of a sensor manager whose distance data is being requested
+
+                Returns:
+                    float: the result of the request being produced by the corresponding property function
+            """
+
+            timestamp = time.time()
+            update_data = sensor_manager.updates[direction]
+
+            # check if the distance value should be updated
+            if update_data["time"] < timestamp - sensor_manager.REFRESH_INTERVAL:
+                # update the distance value and the corresponding timestamp
+                update_data["value"] = sensor.distance * 100
+                update_data["time"] = timestamp
+
+            return func(sensor_manager)
+        
+        return update
+    return wrapper
+
+
 @Singleton
 class SensorManager:
     """ collects information about the distance to obstacles around the vehicle """
 
     REFRESH_INTERVAL = 0.1  # interval in which new values are calculated
     
-    def __init__(self):
-        """ initializes the sensor manager and starts the thread that refreshes the sensor data """
+    # keys for sensor data dictionary
+    FRONT_KEY = "front"
+    RIGHT_KEY = "right"
+    REAR_KEY = "rear"
+    REAR_ANGLED_KEY = "rear_angled"
 
-        self.sensor_front = DistanceSensor(echo=const.EchoPin.FRONT, trigger=const.TriggerPin.FRONT)
-        self.sensor_right = DistanceSensor(echo=const.EchoPin.RIGHT, trigger=const.TriggerPin.RIGHT)
-        self.sensor_back = DistanceSensor(echo=const.EchoPin.BACK, trigger=const.TriggerPin.BACK)
-        self.sensor_back_angled = DistanceSensor(echo=const.EchoPin.BACK_ANGLED, trigger=const.TriggerPin.BACK_ANGLED)
+    # distance sensors
+    SENSOR_FRONT = DistanceSensor(echo=const.EchoPin.FRONT, trigger=const.TriggerPin.FRONT)
+    SENSOR_RIGHT = DistanceSensor(echo=const.EchoPin.RIGHT, trigger=const.TriggerPin.RIGHT)
+    SENSOR_REAR = DistanceSensor(echo=const.EchoPin.BACK, trigger=const.TriggerPin.BACK)
+    SENSOR_REAR_ANGLED = DistanceSensor(echo=const.EchoPin.BACK_ANGLED, trigger=const.TriggerPin.BACK_ANGLED)
+    
+    def __init__(self):
+        """ initializes the sensor manager by defining a dictionary that
+            keeps track of their last update timestamps and values
+        """
 
         self.updates = {
-            "front": {
+            self.FRONT_KEY: {
                 "time": 0,
                 "value": 0
             },
-            "right": {
+            self.RIGHT_KEY: {
                 "time": 0,
                 "value": 0
             },
-            "rear": {
+            self.REAR_KEY: {
                 "time": 0,
                 "value": 0
             },
-            "rear_angled": {
+            self.REAR_ANGLED_KEY: {
                 "time": 0,
                 "value": 0
             }
         }
 
-        front = self.sensor_front.distance * 100
-        print(front)
-
     @property
+    @lazy_sensor_update(FRONT_KEY, SENSOR_FRONT)
     def front(self):
-        direction = "front"
-        if self.updates[direction]["time"] < time.time()-self.REFRESH_INTERVAL:
-            self.updates[direction]["value"] = self.sensor_front.distance * 100
-            self.updates[direction]["time"] = time.time()
-        return self.updates[direction]["value"]   
+        """ determines the distance value of the front sensor
+            NOTE this method can be used as a `property`
+            NOTE this property is being updated automatically (see `lazy_sensor_update`)
+
+            Returns:
+                float: latest measured distance value
+        """
+
+        return self.updates[self.FRONT_KEY]["value"]
 
     @property
+    @lazy_sensor_update(RIGHT_KEY, SENSOR_RIGHT)
     def right(self):
-        direction = "right"
-        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
-            self.updates[direction]["value"] = self.sensor_right.distance * 100
-            self.updates[direction]["time"] = time.time()
-        return self.updates[direction]["value"]
+        """ determines the distance value of the right sensor
+            NOTE this method can be used as a `property`
+            NOTE this property is being updated automatically (see `lazy_sensor_update`)
+
+            Returns:
+                float: latest measured distance value
+        """
+
+        return self.updates[self.RIGHT_KEY]["value"]
 
     @property
+    @lazy_sensor_update(REAR_KEY, SENSOR_REAR)
     def rear(self):
-        direction = "rear"
-        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
-            self.updates[direction]["value"] = self.sensor_back.distance * 100
-            self.updates[direction]["time"] = time.time()
-        return self.updates[direction]["value"]
+        """ determines the distance value of the rear sensor
+            NOTE this method can be used as a `property`
+            NOTE this property is being updated automatically (see `lazy_sensor_update`)
+
+            Returns:
+                float: latest measured distance value
+        """
+
+        return self.updates[self.REAR_KEY]["value"]
 
     @property
+    @lazy_sensor_update(REAR_ANGLED_KEY, SENSOR_REAR_ANGLED)
     def rear_angled(self):
-        direction = "rear_angled"
-        if self.updates[direction]["time"] < time.time() - self.REFRESH_INTERVAL:
-            self.updates[direction]["value"] = self.sensor_back_angled.distance * 100
-            self.updates[direction]["time"] = time.time()
-        return self.updates[direction]["value"]
+        """ determines the distance value of the rear angled sensor
+            NOTE this method can be used as a `property`
+            NOTE this property is being updated automatically (see `lazy_sensor_update`)
+
+            Returns:
+                float: latest measured distance value
+        """
+
+        return self.updates[self.REAR_ANGLED_KEY]["value"]
 
 
 @Singleton
 class Camera:
     """ captures images
-        NOTE this class should be uses as a context manager
+        NOTE this class should be used as a context manager
     """
 
     def __init__(self):
@@ -124,7 +197,7 @@ class Camera:
 
             while self.uses > 0:
                 with picamera.array.PiRGBArray(camera) as frame:
-                    # capture and locally save an image
+                    # capture an image and save it locally
                     camera.capture(frame, "rgb")
                     self.image = frame.array
 
